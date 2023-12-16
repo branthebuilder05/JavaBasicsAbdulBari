84447d329af9af547e6840ee97323e9e0d01500f
branthebuilder05 <30659231+branthebuilder05@users.noreply.github.com>
2023-12-17 02:55:06 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class time
+{
+	public static void main(String[] args) {
+	
+	   System.out.println(System.currentTimeMillis()/1000/60/60/24/365);
+	   System.out.println(Long.MAX_VALUE/1000000000/60/60/24/365);
+	   
+	    
+	}
+}
